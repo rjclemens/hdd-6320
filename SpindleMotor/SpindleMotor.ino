bf397dcbee818050b593bbbe59d78f729671b68e
rjc422 <rjc422@cornell.edu>
2022-12-06 00:13:23 -0500

    rom.py rotates ring bit lists based on maximal voltage spike

--- SpindleMotor/SpindleMotor.ino
+++ SpindleMotor/SpindleMotor.ino
@@ -7,12 +7,12 @@ int servoPin = 5;
 Servo arm;
 
 int speed;
-int arm_angle = 168;
+int arm_angle = 167;
 double tolerance = 10;
-int offset = 500;
+int offset = 450;
 
 //double d = 1804/8; // bit delay of first ring, 225.5
-double d = 1500/8; // bit delay of first ring
+double d = 2550/8; // bit delay of first ring
 int r = 2.73; // radius of first ring
 int bit_rings[] = {8, 14, 20, 26, 32, 38, 44};
 double rotations[] = {7, 4.5, 3.5, 4, 4.5, 5.5, 0};
@@ -22,13 +22,15 @@ int bit_num = 0;
 
 int bits[182] = {};
 int bit_avg[44] = {}; // init to 0, temporarily hold ring reading before average
-int num_rev = 7; // num of cycles to take avg of
+double volts[182] = {};
+double volt_avg[44] = {}; // temp hold volt avg
+int num_rev = 3; // num of cycles to take avg of
 int bit_val;
 
 void jostleMotor(){
   analogWrite(motorPin, 200);
   delay(100);
-  analogWrite(motorPin, 100);
+  analogWrite(motorPin, 150);
 }
 
 void setup() {
@@ -57,12 +59,13 @@ void loop() {
       for(int i=0; i<bit_rings[ring]; i++){
         double h_voltage = analogRead(hallSensorPin); // offset @ 500mV, low -
         
-//        Serial.println("voltage: " + String(h_voltage)); 
+        Serial.println("voltage: " + String(h_voltage)); 
         if(h_voltage > offset + tolerance) { bit_val = 1; }
         else if(h_voltage < offset - tolerance) { bit_val = 0; }
         else { bit_val = 1; } // freespace as 1
   
         bit_avg[i] += bit_val;
+        volt_avg[i] += h_voltage;
         delayTime += bit_delays[ring];
         delay(bit_delays[ring]);
       }
@@ -71,25 +74,33 @@ void loop() {
         Serial.print(String(bval) + " ");
       }
       Serial.println();
+      for(double vval : volt_avg){
+        Serial.print(String(vval) + " ");
+      }
+      Serial.println();
     }
 
     Serial.println("Writing ring " + String(ring) + " to bits array.");
 
     for(int i=0; i<bit_rings[ring]; i++){
-      int avg = bit_avg[i] >= (num_rev+1)/2 ? 1 : 0;
-      bits[bit_num] = avg;
-      Serial.println("BIT: " + String(bit_num) + ", Value:" + String(avg));
+      int b_avg = bit_avg[i] >= (num_rev-1)/2 ? 1 : 0;
+      double v_avg = volt_avg[i]/num_rev;
+      bits[bit_num] = b_avg;
+      volts[bit_num] = v_avg;
+      
+//      Serial.println("BIT: " + String(bit_num) + ", Value:" + String(avg));
       bit_num += 1;
     }
 
     memset(bit_avg,0,sizeof(bit_avg)); // reset bit_avg to 0s
+    memset(volt_avg,0,sizeof(bit_avg)); // reset bit_avg to 0s
     
     Serial.println("Ring " + String(ring) + " delay time: " + String(delayTime));
 //    analogWrite(motorPin, 0);
 
     // update arm for next ring
     Serial.println("writing arm");
-    arm_angle = (arm_angle > 141) ? arm_angle - rotations[ring] : arm_angle;
+    arm_angle = (arm_angle > 137) ? arm_angle - rotations[ring] : arm_angle;
     Serial.println(arm_angle);
     arm.write(arm_angle);
     delay(8*d); // wait full revolution before reading 
@@ -101,6 +112,10 @@ void loop() {
        Serial.println("Read " + String(bit_num) + " bits.");
        for(int i=0; i<182; i++){
          Serial.print(String(bits[i]) + " ");
+       }
+       Serial.println();
+       for(int i=0; i<182; i++){
+         Serial.print(String(volts[i]) + " ");
        }
        analogWrite(motorPin, 0);
        Serial.println();
--- rom.py
+++ rom.py
@@ -4,13 +4,84 @@ import numpy as np
 ROWS = 12
 COLS = 15
 
+bits_per_ring = np.array([8, 14, 20, 26, 32, 38, 44])
+starting_pos = np.array([0, 8, 22, 42, 68, 100, 138])
+soft_mag = np.array([0, 11, 28, 42, 70, 101, 139])
+diff = soft_mag - starting_pos
+print(diff)
+
+
+def rotate(arr, n):  # n=2: [1, 2, 3, 4, 5] -> [3, 4, 5, 1, 2]
+    return arr[n:] + arr[:n]
+
+
+def split_to_rings(bits):
+    arr = []
+    bit_num = 0
+    for num in bits_per_ring:
+        ring = []
+        for _ in range(num):
+            ring.append(bits[bit_num])
+            bit_num += 1
+        arr.append(ring)
+
+    return arr
+
+
+def argmax(arr):
+    print(max(arr))
+    return arr.index(max(arr))
+
+
+def rotate_to_start(bit_list, volt_list):
+    for i, volts in enumerate(volt_list):
+        ind_max_volts = argmax(volts)  # find max index
+
+        # find index of starting ring element
+        st_index = ind_max_volts - diff[i]
+        st_index = st_index % bits_per_ring[i]  # make index positive
+        print(st_index)
+
+        # rotate st_index to beginning
+        bit_list[i] = rotate(bit_list[i], st_index)
+        volts = rotate(volts, st_index)
+        # print(bits[i])
+        # print(volts)
+
+    return bit_list
+
+
+def collapse(bit_list):
+    bits = []
+    for b_list in bit_list:
+        for b in b_list:
+            bits.append(b)
+
+    return bits
+
+
 with open('out.txt', 'r') as f:
-    bits = f.read().split()
+    bits = list(map(int, f.read().split()))
+
+with open('volt.txt', 'r') as f:
+    volts = list(map(float, f.read().split()))
+
+bit_list = split_to_rings(bits)
+volt_list = split_to_rings(volts)
+
+print(bit_list)
+print(volt_list)
+
+bit_list = rotate_to_start(bit_list, volt_list)
+bits_st = collapse(bit_list)
+
+print(bits)
+print(bits_st)
 
 white_row = np.zeros(ROWS)  # white pixel count per row
 
 pxl_list = list(map(lambda x: [0, 0, 0] if x ==
-                '1' else [255, 255, 255], bits))
+                0 else [255, 255, 255], bits_st))
 
 pxls = np.zeros((ROWS, COLS, 3), dtype=np.uint8)
 
@@ -23,4 +94,4 @@ for r in range(ROWS):
 
 print(white_row)
 new_image = Image.fromarray(pxls)
-new_image.save('ard_out12.png')
+new_image.save('ard_out16.png')
